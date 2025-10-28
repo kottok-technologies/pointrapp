c8d588d33c7b9d871dabfb3c9f73a3ef1e00c262
Lilith Prince <lilith.prince@kochcc.com>
2025-10-28 15:10:03 -0500

    more TF done

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,11 @@ module "ECR" {
 
   ecr_repo = "pointrapp"
 }
+
+module "INFRA" {
+  source = "./modules/infra"
+
+  image_uri = var.image_uri
+  environment = var.environment
+  route53_zone_id = var.route53_zone_id
+}
---
+++ terraform/modules/infra/variables.tf
@@ -0,0 +1,16 @@
+variable "environment" {
+  type        = string
+  description = "Environment name (prod/dev)"
+  default     = "dev"
+}
+
+variable "image_uri" {
+  type        = string
+  description = "ECR image URI to deploy"
+}
+
+variable "route53_zone_id" {
+  type        = string
+  description = "Zone ID for pointrapp in R53"
+  default     = "Z021125431208XT5W7T05"
+}
